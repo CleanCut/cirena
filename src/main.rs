fc6606e2f0abcd4319346fe9a1bc9dc785da2d67
Nathan Stocks <cleancut@github.com>
2022-11-01 21:01:01 -0600

    draw a circle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn_bundle(Camera2dBundle::default());
+
+    // Player circle
+    commands.spawn_bundle(MaterialMesh2dBundle {
+        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+        material: materials.add(ColorMaterial::from(Color::BLUE)),
+        ..Default::default()
+    });
 }
